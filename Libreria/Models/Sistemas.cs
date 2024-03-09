c4cd962f47b8b6c8e82fea9f1b4bb4f0743da786
PEDRO VLADIMIR GUZMAN SALAZAR <PEDRO_GUZMAN1@ucne.edu.do>
2024-03-09 13:36:03 -0400

    Agregamos nuestros Modelos

---
+++ Libreria/Models/Sistemas.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Libreria.Models;
+
+public class Sistemas
+{
+    public int SistemaId { get; set; }
+
+    [Required(ErrorMessage = "Campo Obligatorio")]
+    public string? NombreSistema { get; set; }
+
+    [Required(ErrorMessage = "Campo Obligatorio")]
+    public string? DescripcionSistema { get; set; }
+}
---
+++ Libreria/Models/Tickets.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Libreria.Models;
+
+public class Tickets
+{
+    [Key]
+    public int TicketId { get; set; }
+    [Required(ErrorMessage = "El campo fecha es obligatorio")]
+    public DateTime Fecha { get; set; }
+
+    [Required(ErrorMessage = "Requerido")]
+    [ForeignKey("Prioridades")]
+    public int PrioridadId { get; set; }
+
+    [Required(ErrorMessage = "Requerido")]
+    [ForeignKey("Clientes")]
+    public int ClienteId { get; set; }
+
+    [Required(ErrorMessage = "Requerido")]
+    [ForeignKey("Sistemas")]
+    public int SistemaId { get; set; }
+
+    [Required(ErrorMessage = "El campo Solicitado por es obligatorio")]
+    [RegularExpression("^[a-zA-Z ]+$", ErrorMessage = "Solo debe contener letras.")]
+    public string? Solicitadopor { get; set; }
+
+    [Required(ErrorMessage = "El campo Asunto es obligatorio")]
+    public string? Asunto { get; set; }
+
+    [Required(ErrorMessage = "El campo Descripción es obligatorio")]
+    public string? Descripcion { get; set; }
+}
