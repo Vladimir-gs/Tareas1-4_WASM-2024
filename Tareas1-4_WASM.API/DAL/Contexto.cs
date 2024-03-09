59a7f3230b23be5a9967ffdf89c6e03791c0fbae
PEDRO VLADIMIR GUZMAN SALAZAR <PEDRO_GUZMAN1@ucne.edu.do>
2024-03-09 13:43:21 -0400

    Agregamos nuestro Contexto

---
+++ Tareas1-4_WASM.API/DAL/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using Libreria.Models;
+using Microsoft.EntityFrameworkCore;
+namespace Tareas1_4_WASM.API.DAL
+{
+    public class Contexto: DbContext
+    {
+        public DbSet<Prioridades> Prioridades {  get; set; }
+        public DbSet<Sistemas> Sistemas { get; set;}
+        public DbSet<Clientes> Clientes { get; set;}
+        public DbSet<Tickets> Tickets { get; set;}
+        public Contexto(DbContextOptions<Contexto>options): base(options) { }
+    }
+}
